05d0bb431fd3ae55f783044aa970d8f185df83c3
Biendelle Tolentino <53722643+BiendzTolentino@users.noreply.github.com>
2024-10-05 11:32:55 +0800

    Add files via upload

---
+++ deleteacct_complete.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<style>
+    body{
+        background-image: url(blacktechbackgroundbubbles.jpg);
+        background-size: cover;
+    }
+    h1{
+        text-align: center;
+        color: white;
+    }
+    .mes{
+        margin-top: 400px;
+        color: white;
+        text-align: center;
+    }
+    .button1 {
+        margin-top: 20px;
+    }
+    .button1 a{
+        text-decoration: none;
+        background-color: red;
+        color: white;
+        padding-top: 10px;
+        padding-bottom: 10px;
+        padding-left: 20px;
+        padding-right: 20px;
+        border-radius: 8px;
+        margin-left: 580px;
+        margin-right: 580px;
+    }
+
+</style>
+
+<body>
+    <h1>Your Account Has Been Deleted!!</h1>
+    <div class="button1">
+        <a href="Login.php" name="changed_logout_btn" class="btn btn-primary">Go to Login Page</a>
+    </div>
+</body>
+</html>
